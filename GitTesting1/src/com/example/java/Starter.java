eb855a2faa6b2ca9b0fe13423d89920e2d163738
Miles Lin <miles.linyu@gmail.com>
2012-12-19 21:32:32 +0800

    NEW - task : New Task

---
+++ GitTesting1/src/com/example/java/Starter.java
@@ -0,0 +1,16 @@
+package com.example.java;
+
+public class Starter {
+
+	public Starter() {
+		System.out.print("MM\n");
+	}
+	
+	public static void main(String[] args) {
+		
+		Starter starter = new Starter();
+		System.out.print("QQQ");
+		//test_modify1
+	}
+	
+}
